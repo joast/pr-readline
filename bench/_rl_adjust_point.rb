05a38b1919cb04475344cc360a6b781a2d9a7542
Rick Ohnemus <rick_ohnemus@acm.org>
2022-09-11 20:22:14 -0700

    Fixed some rubocop complaints

--- bench/_rl_adjust_point.rb
+++ bench/_rl_adjust_point.rb
@@ -1,3 +1,9 @@
+# warn_indent: true
+# frozen_string_literal: true
+
+# rubocop:disable Lint/MissingCopEnableDirective
+# rubocop:disable Layout/LineLength
+
 $LOAD_PATH.unshift "#{File.dirname(__FILE__)}/../lib/"
 require 'pr-readline'
 require 'benchmark'
@@ -6,21 +12,26 @@ N = 100_000
 
 Benchmark.bmbm do |x|
   x.report do
-    N.times { PrReadline._rl_adjust_point("a", 0) }
+    N.times { PrReadline._rl_adjust_point('a', 0) }
   end
+
   x.report do
-    N.times { PrReadline._rl_adjust_point("a", 1) }
+    N.times { PrReadline._rl_adjust_point('a', 1) }
   end
+
   x.report do
-    N.times { PrReadline._rl_adjust_point("aaaaaaaaaaaaaaaaaaaaa", 0) }
+    N.times { PrReadline._rl_adjust_point('aaaaaaaaaaaaaaaaaaaaa', 0) }
   end
+
   x.report do
-    N.times { PrReadline._rl_adjust_point("aaaaaaaaaaaaaaaaaaaaa", 40) }
+    N.times { PrReadline._rl_adjust_point('aaaaaaaaaaaaaaaaaaaaa', 40) }
   end
+
   x.report do
-    N.times { PrReadline._rl_adjust_point("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 0) }
+    N.times { PrReadline._rl_adjust_point('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 0) }
   end
+
   x.report do
-    N.times { PrReadline._rl_adjust_point("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 40) }
+    N.times { PrReadline._rl_adjust_point('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 40) }
   end
 end
