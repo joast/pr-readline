9e04645c114569def096441a992c1ed616ddbe3a
Rick Ohnemus <rick_ohnemus@acm.org>
2022-09-29 15:00:36 -0700

    duh!

--- test/test_init_files.rb
+++ test/test_init_files.rb
@@ -1,18 +1,25 @@
 # warn_indent: true
 # frozen_string_literal: true
 
+# rubocop:disable Lint/MissingCopEnableDirective
+# rubocop:disable Layout/LineLength
+
 require_relative 'test_helper'
 require 'pr-readline'
 
-class TestCapabilities < Minitest::Test # :nodoc:
-  def test_init_files
-    oldpwd = Dir.pwd
+class TestInitFiles < Minitest::Test # :nodoc:
+  def setup
+    @run_dir = Dir.pwd
     Dir.chdir(__dir__)
+  end
 
+  def test_init_files
     assert_output('', "readline: init_files/circular_1: already read or circular include\n") do
       PrReadline.rl_read_init_file('init_files/circular_1')
     end
-  ensure
-    Dir.chdir(oldpwd)
+  end
+
+  def teardown
+    Dir.chdir(@run_dir)
   end
 end
